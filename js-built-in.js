363870b29d998fef208e72d0eb32fe6d81a3dc24
Dinh Van <trankyvan08@gmail.com>
2024-02-19 17:08:08 +0700

    learn javascript

---
+++ js-built-in.js
@@ -0,0 +1,24 @@
+ 
+// 1. Alert 
+alert("Tran Dinh Van");
+
+// 2.Confirm 
+confirm("Ban co phai la nam khong ?"); 
+
+// 3.Cosole.log
+console.log("Tran Dinh Van la mot Dev chuyen nghiep");
+
+// 4.Prompt
+prompt("Ho va ten ban la gi");
+
+// 5.setTimeout
+setTimeout(function() {
+    confirm("Toi la Api");
+},5000) 
+// chay 1 lan va 1s = 1000
+
+// 6.setInterval
+setInterval(function(){
+    console.log("Tran Dinh Van");
+},3000) 
+// chay lien tuc va lap lai.
---
+++ js-operators.js
@@ -0,0 +1,21 @@
+/**
+ Giới thiệu về các toán tử trong Js
+ 1. Toán tử số học - Arithmetic
+    + Cộng
+    - Trừ 
+    * Nhân
+    / Chia
+    ** Lũy thừa
+    % Chia lấy số dư
+    ++ Tăng một giá trị
+    -- Giảm một giá trị
+ 2. Toán tử gán - Assignment
+ 3. Toán tử so sánh - Comparison
+ 4. Toán tử logic - Logical
+ 5. Toán tử ++ , -- as Prefix & Postfix
+    vd: --a là tiền tố (Prefix) , a-- là hậu tố (Postfix)
+ */
+
+let a = 7;
+a--;
+console.log(a);
